2bf485e65d8951ec34e28a5b209efd1928224163
Josh Silvia <jsilvia721@gmail.com>
2020-07-26 17:12:31 -0400

    Add setup script, check if binary exists before running server

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,4 +4,10 @@ set -e
 
 ./scripts/swagger-gen.sh
 
+if ! command -v todo-list-server &> /dev/null
+then
+    echo "todo-list-server could not be found, installing binary"
+    go install ./cmd/todo-list-server/
+fi
+
 todo-list-server --port=12345
